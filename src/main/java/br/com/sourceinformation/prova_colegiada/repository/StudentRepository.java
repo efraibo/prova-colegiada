56042d737b578050c00c0f7f3a01ac9cca13026d
Evandro <efraibo@gmail.com>
2018-12-19 13:12:00 -0300

    Excluindo entidade Student da aplicacao

--- src/main/java/br/com/sourceinformation/prova_colegiada/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.sourceinformation.prova_colegiada.repository;
-
-import br.com.sourceinformation.prova_colegiada.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
